14dbb6ed8b6c6339611fdc19fcef4f5cd4b23c01
John Carter <36883870+jcrtr@users.noreply.github.com>
2024-02-27 15:44:46 +0300

    Update Package.swift
    fix

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Libbox",
-      url: "https://github.com/EbrahimTahernejad/sing-box-lib/releases/download/1.8.5/Libbox.xcframework.zip",
+      url: "https://github.com/jcrtr/sing-box-lib/releases/download/1.8.5/Libbox.xcframework.zip",
       checksum: "bcb2e4b26bc53b77b6817806cf57c244113b980f5e404717f1009489f6e48730"
     )
   ]
