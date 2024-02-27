5436c74ad577bcb0b8e34696df47a9784b279514
jcrtr <jcrtr@users.noreply.github.com>
2024-02-27 13:02:16 +0000

    Libbox Release v1.8.6

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Libbox",
-      url: "https://github.com/jcrtr/sing-box-lib/releases/download/1.8.5/Libbox.xcframework.zip",
-      checksum: "bcb2e4b26bc53b77b6817806cf57c244113b980f5e404717f1009489f6e48730"
+      url: "https://github.com/jcrtr/sing-box-lib/releases/download/v1.8.6/Libbox.xcframework.zip",
+      checksum: "c30eee141dc58d8ee89fc3348b3c823de671522fb263c4c5314e18b1a736dfc6"
     )
   ]
 )
